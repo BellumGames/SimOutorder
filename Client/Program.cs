041e1831b6bc05383acc454b4c193adaeaf9ab9d
ctimar45@yahoo.com <ctimar45@yahoo.com>
2023-04-20 19:56:16 +0300

    added the BuildCommand class

--- Client/Program.cs
+++ Client/Program.cs
@@ -7,6 +7,8 @@ namespace Client
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            BuildSimCommand simCommand = new BuildSimCommand();
+            simCommand.GetPropertis();
         }
     }
 }
