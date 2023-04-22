560681091889eb37b2ebf668ed6c750e40f8ede1
ctimar45@yahoo.com <ctimar45@yahoo.com>
2023-04-22 20:30:57 +0300

    finished the parse/extract metrics

--- Client/bin/Debug/simplesim-3.0/script.sh
+++ Client/bin/Debug/simplesim-3.0/script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 chmod 777 script.sh
 cd /home/timarc/Desktop/MareProiect/SimOutorder/Client/bin/Debug/simplesim-3.0
-./sim-outorder -redir:sim results/gcc_1024.res -fastfwd 1000000 -max:inst 10000000 -lvpt:size 1024 benchmarks/applu.ss < benchmarks/applu.in
+./sim-outorder -redir:sim results/simulation.txt -redir:prog results/applu_progout.res -max:inst 10000 -fastfwd 0 -fetch:ifqsize 4 -bpred:bimod 2048 -decode:width 4 -issue:width 4 -commit:width 4 -ruu:size 16 -lsq:size 8 -cache:dl1 dl1:128:32:4:l -mem:lat 18 2 -tlb:itlb itlb:16:4096:4:l -res:ialu 4 benchmarks/applu.ss < benchmarks/applu.in
