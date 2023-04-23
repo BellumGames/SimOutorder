9520e0e78f9c5d8b939fb6a47b7b4c3e530b38bb
ctimar45@yahoo.com <ctimar45@yahoo.com>
2023-04-23 16:50:10 +0300

    client done for good

--- Client/Program.cs
+++ Client/Program.cs
@@ -12,15 +12,15 @@ namespace Client
         private static readonly string PATH_TO_SCRIPT = "../Debug/simplesim-3.0/script.sh";
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Net();
-            //LaunchSim();
+            Console.WriteLine("Welcome to 'Defenetly not a virus team'.");
+            //Net();
+            LaunchSim();
         }
         public static void GenerateScript()
         {
             string commnandBuffer = "./sim-outorder -redir:sim results/simulation.res -redir:prog results/applu_progout.res -max:inst 10000 -fastfwd 0 -fetch:ifqsize 4 -bpred:bimod 2048 -decode:width 4 -issue:width 4 -commit:width 4 -ruu:size 16 -lsq:size 8 -cache:dl1 dl1:128:32:4:l -mem:lat 18 2 -tlb:itlb itlb:16:4096:4:l -res:ialu 4 benchmarks/applu.ss < benchmarks/applu.in";
 
-            File.WriteAllText(PATH_TO_SCRIPT, "#!/bin/bash\nchmod 777 script.sh\ncd /home/bellum/Projects/SimOutorder/Client/bin/Debug/simplesim-3.0/\n" + commnandBuffer);
+            File.WriteAllText(PATH_TO_SCRIPT, "#!/bin/bash\nchmod 777 script.sh\ncd /home/timarc/Desktop/MareProiect/SimOutorder/Client/bin/Debug/simplesim-3.0/\n" + commnandBuffer);
             File.Exists(PATH_TO_SCRIPT);
         }
 
@@ -34,8 +34,8 @@ namespace Client
             proc.Start();
 
             Metrics metrics = new Metrics();
-            metrics.parseString("../Debug/simplesim-3.0/results/simulation.res");
-
+            metrics = metrics.parseString("../Debug/simplesim-3.0/results/simulation.res");
+            Console.WriteLine("Everiting is done, go in peace my son.");
         }
 
         public static void Net()
